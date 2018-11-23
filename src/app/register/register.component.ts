84212448f25b3bba5422bb93604a2800e7ee7e4b
jlong11235 <jlong11235@gmail.com>
2018-11-23 01:31:09 +0000

    auth and registration controls

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {register} from "ts-node";
+import {AuthService} from "../_services/auth.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register(){
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successful');
+    }, error => {
+      console.log('registration error');
+    });
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+}
